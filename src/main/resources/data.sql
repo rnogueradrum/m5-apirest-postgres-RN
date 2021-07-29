d938ba4a3ad79a72342f0871a398b810da318ce6
Ricardo <rnogueradrum@gmail.com>
2021-07-29 09:37:57 +0200

    commit data.sql OK

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO cars (manufacturer, model, cc, doors) VALUES ('Honda','Jazz',1.2,3);
+INSERT INTO cars (manufacturer, model, cc, doors) VALUES ('Rolls Royce','Silver Ghost',19.5,6);
+INSERT INTO cars (manufacturer, model, cc, doors) VALUES ('Ferrari','Testarossa',9.2,3);
+INSERT INTO cars (manufacturer, model, cc, doors) VALUES ('Tesla','Electra',1.5,3);
